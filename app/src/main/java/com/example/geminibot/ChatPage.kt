185b88332b0f90eb0e0fc45cc090c812c5331365
Chidu38 <chidupanku38@gmail.com>
2026-05-09 15:32:43 +0530

    all compleated

--- app/src/main/java/com/example/geminibot/ChatPage.kt
+++ app/src/main/java/com/example/geminibot/ChatPage.kt
@@ -1,15 +1,93 @@
 package com.example.geminibot
 
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ChatScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Chat page",
-        modifier = modifier,
-        style = MaterialTheme.typography.headlineLarge
-    )
+
+    val viewmodel: ChatViewmodel = ChatViewmodel()
+    val message = viewmodel.messages
+
+    Column(modifier = modifier
+        .fillMaxSize()
+    ) {
+        LazyColumn(
+            modifier = Modifier.padding(8.dp)
+                .weight(1f)
+        ) {
+            items(message) {txt ->
+                MessageBubble(txt)
+            }
+        }
+
+        MessageInput(
+            onSend = {
+                viewmodel.sendMessage(it)
+            }
+        )
+    }
+}
+
+@Composable
+fun MessageBubble(msg: Message) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalArrangement = if (msg.user) Arrangement.End else Arrangement.Start
+    ) {
+        Text(
+            text = msg.text,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Composable
+fun MessageInput(onSend: (String) -> Unit) {
+    var message by remember { mutableStateOf("") }
+
+    Row(modifier = Modifier.padding(8.dp)) {
+        OutlinedTextField(
+            modifier = Modifier.weight(1f),
+            value = message,
+            onValueChange = {
+                message = it
+            },
+            shape = RoundedCornerShape(15.dp),
+            placeholder = {Text(text = "Ask anything...")},
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        onSend(message)
+                        message = ""
+                    }
+                ) {
+                    Icon(Icons.Default.Send, contentDescription = "send")
+                }
+            }
+        )
+    }
 }
--- app/src/main/java/com/example/geminibot/ChatViewmodel.kt
+++ app/src/main/java/com/example/geminibot/ChatViewmodel.kt
@@ -1,9 +1,46 @@
 package com.example.geminibot
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.Firebase
+import com.google.firebase.ai.ai
+import com.google.firebase.ai.type.GenerativeBackend
+import kotlinx.coroutines.launch
 
-class ChatViewmodel: ViewModel() {
-    fun sendMessage() {
+data class Message(val user: Boolean, val text: String)
 
+class ChatViewmodel : ViewModel() {
+    val model = Firebase.ai(backend = GenerativeBackend.googleAI())
+        .generativeModel("gemini-2.5-flash")
+    val messages = mutableStateListOf<Message>()
+    fun sendMessage(question: String) {
+        val chatHistory = buildHistory(messages)
+        val systemPrompt = """
+        You are an helpful Ai assistant named IRON-MAN, Your creator is Tejas,
+         only reveal your identity if user ask, Now your only job is to 
+         response this prompt : $question, refer history : $chatHistory
+    """.trimIndent()
+
+        viewModelScope.launch {
+            messages.add(Message(text = question, user = true))
+            val response = model.generateContent(systemPrompt)
+            val answer = response.text ?: "No response"
+            messages.add(Message(text = answer, user = false))
+        }
     }
+}
+
+fun buildHistory(messages: List<Message>): String {
+    val history = StringBuilder()
+
+    for (msg in messages){
+        if (msg.user){
+            history.append("User : ${msg.text}")
+        }else{
+            history.append("AI : ${msg.text}")
+        }
+    }
+
+    return history.toString()
 }
